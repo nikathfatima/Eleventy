48ee10eac6bd922b5a72f8fbce101350e134421e
Good Name <genuine.kmu@gmail.com>
2020-07-19 05:25:08 +0530

    Starting my first site with Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+
+    config.addCollection('posts', collection => {
+        return collection.getFilteredByGlob('src/site/posts/*.md');
+    });
+
+    return {
+        dir: {
+            input: "src/site" ,
+            output: "dist",
+            includes: "templates"
+       },
+       templateFormats : ["html", "md"],
+       htmlTemplateEngine : "liquid",
+       markdownTemplateEngine  : "liquid"
+    };
+}
---
+++ src/site/posts/welcome-post.md
@@ -0,0 +1,8 @@
+---
+title: "Hello worldy! how are you"
+date: "2020-07-17"
+---
+
+## This is an h2
+
+This is our paragraph and **it has bold**
